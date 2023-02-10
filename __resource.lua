08b69f5be642e556cf237d7c74466d8ac066dea3
BlackHawk <85005142+hi764@users.noreply.github.com>
2023-02-10 14:30:54 -0600

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,11 @@
------------------------------------
----------- Discord Reports --------
----           by Badger         ---
------------------------------------
+-----------------------------------------------------
+-- Discord Reports by Badger | Re - Coded by hi764 --
+-----------------------------------------------------
+fx_version 'adamant'
+game 'gta5'
 
+-------------------------------------
+-- Start of the files, do NOT edit --
+-------------------------------------
 client_script "client.lua"
 server_script "server.lua"
